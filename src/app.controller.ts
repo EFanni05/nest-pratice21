fbfc91cbbccec9d7cfe65a1c862a1d64b73862a2
Fannichan <elekes.fanni2005@gmail.com>
2023-11-21 14:25:41 +0100

    email+password

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import e from 'express';
+
+const regex = /[A-Za-z0-9]{8,}/
+const regemail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Render('urlap')
+  getHello(
+    @Query('email') email: string,
+    @Query('password') password: string,
+    @Query('password1') password1: string
+  ): object {
+    const errors: any = {};
+    if(email ===  '') {
+      errors.email = "Üres az email mező!"
+    }
+    if(!regemail.test(email)){
+      errors.email = "Hibás email formátum"
+    }
+     if (!(password == password1)) {
+       errors.password = 'A jelszók nem egyeznek';
+     }
+    if(!regex.test(password) && !regex.test(password1)){
+      errors.password = "A jelszó nem fele meg"
+    }
+    return { email, errors, negyedik: 12 };
   }
 }
